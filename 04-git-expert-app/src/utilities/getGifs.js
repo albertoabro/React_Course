387159ae981cf12cbf1bdad7c9b646b59bcedb9f
Alberto Abad <114659686+albertoabro@users.noreply.github.com>
2025-01-17 10:47:21 +0100

    Update getGifs.js

--- 04-git-expert-app/src/utilities/getGifs.js
+++ 04-git-expert-app/src/utilities/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const apiKey = 'Keeux9fZo305sabIAyfJnx5pqqxeFTBG';
+    const apiKey = ''; //insert your Api Key from api.giphy
     const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=1`;
     const response = await fetch(url);
     const {data} = await response.json();
