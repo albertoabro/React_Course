a575da1a0255f2c44c95da33ba4fa4495ace253b
Alberto <albertoabro@gmail.com>
2025-01-19 13:27:01 +0100

    Add AuthLayout to LoginPage and RegisterPage

--- 07-journal-app/src/auth/pages/LoginPage.jsx
+++ 07-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,15 +1,56 @@
-import { Grid2 } from "@mui/material"
+import { Link as RouterLink } from 'react-router';
+import { Google } from "@mui/icons-material"
+import { Button, Grid2, TextField, Typography, Link } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout';
 
 
 export const LoginPage = () => {
     return (
-      <Grid2 
-      alignItems="center"
-      container
-      direction="column"
-      justifyContent="center"
-      sx={{ minHeight:"100vh", backgroundColor: "primary.main", padding: 4 }}
-      >
-      </Grid2>
+
+     <AuthLayout title='Login'>
+        
+        <form>
+            <Grid2 container>
+              <Grid2 size={{ sm: 12, md: 12, lg: 6 }} sx={{ mt: 2 }}>
+                <TextField 
+                  label="email"
+                  type="email"
+                  placeholder="email@gmail.com"
+                  fullWidth
+                />
+              </Grid2>
+
+              <Grid2 size={{ sm: 12, md: 12, lg: 6 }} sx={{ mt: 2 }}>
+                <TextField 
+                  label="Password"
+                  type="password"
+                  placeholder="password"
+                  fullWidth
+                />
+              </Grid2>
+
+              <Grid2 container size={12} spacing={2} sx={{ mb: 2, mt: 1}}>
+                <Grid2 size={{xs: 12, sm: 12, md: 6}}>
+                  <Button variant="contained" fullWidth>
+                    Login
+                  </Button>
+                </Grid2>
+
+                <Grid2 size={{xs: 12, sm: 6, md: 6}}>
+                  <Button variant="contained" fullWidth>
+                    <Google />
+                    <Typography size={{ml:1}}>Google</Typography>
+                  </Button>
+                </Grid2>
+              </Grid2>
+
+            <Grid2 container direction='row' justifyContent='end' size={12}>
+              <Link color="inherit" to='/auth/register' component={RouterLink}>
+                Sign up
+              </Link>
+            </Grid2>
+          </Grid2>
+        </form>
+      </AuthLayout>
     )
   }
--- 07-journal-app/src/auth/pages/RegisterPage.jsx
+++ 07-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,59 @@
+import { Link as RouterLink } from 'react-router';
+import { Button, Grid2, TextField, Typography, Link } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout';
+
 
 export const RegisterPage = () => {
     return (
-      <>
-          
-      </>
+
+     <AuthLayout title='Sign Up'>
+        
+        <form>
+            <Grid2 container>
+
+              <Grid2 size={12} sx={{ mt: 2 }}>
+                <TextField 
+                  label="Full name"
+                  type="text"
+                  placeholder="Name"
+                  fullWidth
+                />
+              </Grid2>
+
+              <Grid2 size={12} sx={{ mt: 2 }}>
+                <TextField 
+                  label="email"
+                  type="email"
+                  placeholder="email@gmail.com"
+                  fullWidth
+                />
+              </Grid2>
+
+              <Grid2 size={12} sx={{ mt: 2 }}>
+                <TextField 
+                  label="Password"
+                  type="password"
+                  placeholder="password"
+                  fullWidth
+                />
+              </Grid2>
+
+              <Grid2 container size={12} spacing={2} sx={{ mb: 2, mt: 1}}>
+                <Grid2 size={12}>
+                  <Button variant="contained" fullWidth>
+                    Sign Up
+                  </Button>
+                </Grid2>
+              </Grid2>
+
+            <Grid2 container direction='row' justifyContent='end' size={12}>
+              <Typography sx={{mr: 1}}>Have you account?</Typography>
+              <Link color="inherit" to='/auth/login' component={RouterLink}>
+                Sign in
+              </Link>
+            </Grid2>
+          </Grid2>
+        </form>
+      </AuthLayout>
     )
   }
