50b920cfaf9dffabbcdc87b440db6c14e4b6a95b
Alberto Abad <114659686+albertoabro@users.noreply.github.com>
2025-02-13 13:22:58 +0100

    Delete src directory

--- src/actions/get-planets.action.ts
+++
@@ -1,7 +0,0 @@
-import { planetsApi } from "../api/planetsApi";
-import type { Planet } from "../interfaces/planet.interface";
-
-export const getPlanets = async (): Promise<Planet[]> => {
-  const res = await planetsApi.get<Planet[]>('/');
-  return res.data;
-};
--- src/interfaces/planet.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Planet {
-  id: string;
-  name: string;
-  type: string;
-  distanceFromSun: string;
-}
